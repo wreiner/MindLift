210b56eb3658eeaf1795039b61d8f8353e2854d6
Prager Kilian <kilian.prager@edu.fh-joanneum.at>
2024-06-24 15:12:28 +0200

    remove unused annotation

--- MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/util/GetCopingStrategy.kt
+++ MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/util/GetCopingStrategy.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,7 +23,6 @@ import eu.sumindlift.mindlift.data.entity.CopingStrategy
 import eu.sumindlift.mindlift.data.entity.EnergyLevel
 import eu.sumindlift.mindlift.ui.viewmodel.GetCopingStrategyViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GetCopingStrategy(
     modifier: Modifier = Modifier,
