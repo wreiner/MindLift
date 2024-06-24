7bdc5120bafed0d42ee3a8098601c067b25f21e7
Prager Kilian <kilian.prager@edu.fh-joanneum.at>
2024-06-24 15:05:10 +0200

    remove unused annotation and simplify condition

--- MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/viewmodel/GetCopingStrategyViewModel.kt
+++ MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/viewmodel/GetCopingStrategyViewModel.kt
@@ -8,13 +8,11 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import eu.sumindlift.mindlift.data.entity.CopingStrategy
 import eu.sumindlift.mindlift.data.repository.CopingStrategyRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class GetCopingStrategyViewModel @Inject constructor(private val repository: CopingStrategyRepository) : ViewModel() {
 
@@ -28,9 +26,7 @@ class GetCopingStrategyViewModel @Inject constructor(private val repository: Cop
         viewModelScope.launch {
             _onLoading = true
             val newStrategy = repository.getRandomCopingStrategyWithEnergyLevel(energyLevel)
-            if (newStrategy != null) {
-                _strategy.value = newStrategy
-            }
+            _strategy.value = newStrategy
             _onLoading = false
         }
     }
