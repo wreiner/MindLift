9a459411a98d3c31b66e61e1aee87c71f4ab85c4
Walter Reiner <walter.reiner@wreiner.at>
2024-06-24 10:41:28 +0200

    Add a placeholder when there are now coping strategies saved yet

--- MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/util/CopingStrategiesList.kt
+++ MindLift/app/src/main/java/eu/sumindlift/mindlift/ui/util/CopingStrategiesList.kt
@@ -1,19 +1,24 @@
 package eu.sumindlift.mindlift.ui.util
 
+import android.widget.Space
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -21,9 +26,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import eu.sumindlift.mindlift.R
 import eu.sumindlift.mindlift.data.entity.CopingStrategy
 import eu.sumindlift.mindlift.data.entity.EnergyLevel
 import eu.sumindlift.mindlift.data.repository.CopingStrategyRepository
+import eu.sumindlift.mindlift.ui.navigation.Screens
 
 @Composable
 fun CopingStrategiesList(
@@ -33,15 +40,45 @@ fun CopingStrategiesList(
 ) {
     val copingStrategiesList by copingStrategyRepository.allCopingStrategies.collectAsState(initial = emptyList())
 
-    LazyColumn(
-        modifier = modifier
+    if (copingStrategiesList.isEmpty()) {
+        CopingStrategyListEmptyPlaceholder(navController, modifier)
+    } else {
+        LazyColumn(
+            modifier = modifier
+        ) {
+            items(copingStrategiesList) { copingStrategy ->
+                CopingStrategyListCard(
+                    navController = navController,
+                    copingStrategy = copingStrategy,
+                    modifier = Modifier.padding(2.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CopingStrategyListEmptyPlaceholder(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        items(copingStrategiesList) { copingStrategy ->
-            CopingStrategyListCard(
-                navController = navController,
-                copingStrategy = copingStrategy,
-                modifier = Modifier.padding(2.dp)
-            )
+        Text(
+            text = stringResource(id = R.string.coping_strategy_list_empty_placeholder_text)
+        )
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate(Screens.AddCopingStrategy.route)
+            }
+        ) {
+            Text(text = stringResource(id = R.string.coping_strategy_list_empty_placeholder_add_button_text))
         }
     }
 }
